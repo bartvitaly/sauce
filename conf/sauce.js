68540b0f334d0ecf75dcfdfec51fa65cd73397b6
Vitalii Bartashchuk <bartvitaly@gmail.com>
2016-08-30 09:58:27 +0200

    enabled chrome

--- conf/sauce.js
+++ conf/sauce.js
@@ -12,7 +12,7 @@ exports.config = {
 
 	capabilities : {
 		'newCommandTimeout' : timeout / 1000,
-		'browserName' : 'Browser',
+		'browserName' : 'Chrome',
 		'deviceName' : 'test',
 		'platformName' : 'Android',
 		'platformVersion' : '5.1.1',
