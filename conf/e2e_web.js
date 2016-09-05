2cea1c3a5e54ac5dc0d8d7bea833f0ca4f273bdb
Vitalii Bartashchuk <bartvitaly@gmail.com>
2016-09-05 19:59:53 +0200

    added e2e: pageloadtimeout -

--- conf/e2e_web.js
+++ conf/e2e_web.js
@@ -44,7 +44,7 @@ exports.config = {
 			}));
 			browser.ignoreSynchronization = true;
 			browser.manage().timeouts().setScriptTimeout(timeout);
-			browser.manage().timeouts().pageLoadTimeout(timeout);
+//			browser.manage().timeouts().pageLoadTimeout(timeout);
 			browser.manage().timeouts().implicitlyWait(timeout);
 			browser.manage().deleteAllCookies();
 		});
