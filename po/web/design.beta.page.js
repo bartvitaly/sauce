688ccf6b17b9e357bdd3e8f920e29dcf5c79445e
Vitalii Bartashchuk <bartvitaly@gmail.com>
2016-09-05 21:49:50 +0200

    added e2e: pageloadtimeout -

--- po/web/design.beta.page.js
+++ po/web/design.beta.page.js
@@ -1,77 +1,89 @@
 var commonFunctions = require('../../common/commonWeb.js');
 
-var designBetaPage = function () {
+var designBetaPage = function() {
 
 	this.addTextButton = by.css(".add_item_text");
 	this.editTextInput = by.css("[id='options-add_item_text'] textarea");
 	this.XOnForm = by.css('.glyphicons.remove_2.glyphicons-12.pull-right');
 	this.showBackButton = by.xpath('.//*[@class="box-thumb btn-show-back pull-left"]/span[contains(text(),"Back")]');
 	this.showFrontButton = by.xpath('.//*[@class="box-thumb btn-show-back pull-left"]/span[contains(text(),"Front")]');
-	
-	this.nextButton = by.css(".product-prices .green-meadow");
-	
+
+	this.nextButton = by.css(".step2"); // .product-prices .green-meadow
+
 	this.chooseProductButton = by.css(".menu-left>li:first-child");
 	this.clearAll = by.css(".intercom-notifications-dismiss-button");
 
-	this.chooseAProductCategory = function(productCategory){
-/*		browser.wait(new protractor.until.Condition('some condition', function () {
-			return browser.driver.findElement(by.xpath('(.//*[text()="' + productCategory + '"])[1]')).isDisplayed();
-		}), 20000).then(function(){
-			browser.driver.findElement(by.xpath('(.//*[text()="' + productCategory + '"])[1]')).click();
-		})*/
-		browser.driver.wait(function () {
+	this.chooseAProductCategory = function(productCategory) {
+		/*
+		 * browser.wait(new protractor.until.Condition('some condition',
+		 * function () { return
+		 * browser.driver.findElement(by.xpath('(.//*[text()="' +
+		 * productCategory + '"])[1]')).isDisplayed(); }),
+		 * 20000).then(function(){
+		 * browser.driver.findElement(by.xpath('(.//*[text()="' +
+		 * productCategory + '"])[1]')).click(); })
+		 */
+		browser.driver.wait(function() {
 			return browser.driver.isElementPresent(by.xpath('(.//*[text()="' + productCategory + '"])[1]'));
 		}, 20000);
 		browser.driver.findElement(by.xpath('(.//*[text()="' + productCategory + '"])[1]')).click();
 	};
 
-	this.chooseAProduct = function(product){
+	this.chooseAProduct = function(product) {
 		element(by.css('#mCSB_2_container>div:nth-of-type(1)')).click();
 		browser.driver.actions().sendKeys(protractor.Key.PAGE_DOWN).perform();
-		browser.driver.wait(function () {
-			return browser.driver.isElementPresent(by.xpath('.//*[text()="' + product + '"]/following-sibling::div[@class="row"]//button[text()="Select"]'));
+		browser.driver.wait(function() {
+			return browser.driver.isElementPresent(by.xpath('.//*[text()="' + product
+					+ '"]/following-sibling::div[@class="row"]//button[text()="Select"]'));
 		}, 20000);
-		browser.driver.findElement(by.xpath('.//*[text()="' + product + '"]/following-sibling::div[@class="row"]//button[text()="Select"]')).click();
-/*		browser.wait(new protractor.until.Condition('some condition', function () {
-			return browser.driver.findElement(by.xpath('.//*[text()="' + product + '"]/following-sibling::div[@class="row"]//button[text()="Select"]')).isDisplayed();
-		}), 20000).then(function(){
-			browser.driver.findElement(by.xpath('.//*[text()="' + product + '"]/following-sibling::div[@class="row"]//button[text()="Select"]')).click();
-		})*/
+		browser.driver.findElement(
+				by
+						.xpath('.//*[text()="' + product
+								+ '"]/following-sibling::div[@class="row"]//button[text()="Select"]')).click();
+		/*
+		 * browser.wait(new protractor.until.Condition('some condition',
+		 * function () { return
+		 * browser.driver.findElement(by.xpath('.//*[text()="' + product +
+		 * '"]/following-sibling::div[@class="row"]//button[text()="Select"]')).isDisplayed();
+		 * }), 20000).then(function(){
+		 * browser.driver.findElement(by.xpath('.//*[text()="' + product +
+		 * '"]/following-sibling::div[@class="row"]//button[text()="Select"]')).click(); })
+		 */
 	};
 
-	this.clickAddTextButton = function (){
+	this.clickAddTextButton = function() {
 		commonFunctions.click(this.addTextButton);
 	};
 
-	//---form
-	this.editText = function (text) {
+	// ---form
+	this.editText = function(text) {
 		commonFunctions.sendKeys(this.editTextInput, text);
 	};
 
-	this.clickXOnForm = function () {
+	this.clickXOnForm = function() {
 		commonFunctions.click(this.XOnForm);
 	};
-    //form---
+	// form---
 
-    this.clickShowBackButton = function () {
-    	commonFunctions.click(this.showBackButton);    	
-    };
+	this.clickShowBackButton = function() {
+		commonFunctions.click(this.showBackButton);
+	};
 
-    this.clickShowFrontButton = function () {
-    	commonFunctions.click(this.showFrontButton);
-    };
+	this.clickShowFrontButton = function() {
+		commonFunctions.click(this.showFrontButton);
+	};
 
-    this.clickNextButton = function () {
-    	commonFunctions.click(this.nextButton);    	
-    };
+	this.clickNextButton = function() {
+		commonFunctions.click(this.nextButton);
+	};
 
-    this.clickChooseProductButton = function () {    	
-    	commonFunctions.click(this.chooseProductButton);
-    };
+	this.clickChooseProductButton = function() {
+		commonFunctions.click(this.chooseProductButton);
+	};
 
-    this.clickClearAllButton  = function() {
-    	commonFunctions.click(this.clearAll);
-    };
+	this.clickClearAllButton = function() {
+		commonFunctions.click(this.clearAll);
+	};
 }
 
 module.exports = new designBetaPage();
